ccf9fad9ef34250d51b57a63e79f30cea385e96c
santhosh-sf-4035 <santhoshkumar.s@syncfusion.com>
2025-11-14 19:41:01 +0530

    Android sample

--- bolddesksdk_android_sample/settings.gradle.kts
+++ bolddesksdk_android_sample/settings.gradle.kts
@@ -24,10 +24,10 @@ dependencyResolutionManagement {
 rootProject.name = "bolddeskSDK_android_sample"
 include(":app")
 
-includeBuild("../../bolddesk-mobile-sdk-android") {
-    dependencySubstitution {
-        substitute(module("io.github.somaprasanna4037:bds-desk")).using(project(":app"))
-    }
-}
+//includeBuild("../../bolddesk-mobile-sdk-android") {
+//    dependencySubstitution {
+//        substitute(module("io.github.somaprasanna4037:bds-desk")).using(project(":app"))
+//    }
+//}
 
  
